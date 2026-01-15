e64504b2056e7f9857614927788dd54432c27004
Izaan <izaan@Izaans-MacBook-Pro.local>
2026-01-14 21:38:04 -0600

    Changed allowed origins

--- api/app.py
+++ api/app.py
@@ -8,10 +8,10 @@ app = FastAPI(title="Scouting Report API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # tighten later
+    allow_origins=["https://scouting-report-frontend.vercel.app/"],  # tighten later
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 
